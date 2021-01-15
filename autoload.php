fa00534b419d1d30273ad8d01c67ab2e6f1ef027
Leandro Zanin <sr.leandrozanin@gmail.com>
2021-01-15 11:45:38 -0300

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -1,3 +1,9 @@
+/**
+ * @package     Test_Shipping
+ * @author      Test
+ * @copyright   Test - https://www.test.com.br
+ * @license     https://opensource.org/licenses/AFL-3.0  Academic Free License 3.0 | Open Source Initiative
+ */
 <?php
 
 
